f06a281c0e0c9cf5b344e74d2c19134a406b0ffd
desarrollador3 <desarrollo@tecnologiasadministrativas.com>
2025-06-20 17:02:20 -0600

    Integracion de Vite al proyecto

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+// src/main.jsx
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import App from "./App";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
