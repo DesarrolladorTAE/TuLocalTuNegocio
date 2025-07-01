e732dda4a0660cc1ebd7742ae417a185b7b6832b
DesarrolladorTAE <desarrollo@tecnologiasadministrativas.com>
2025-07-01 07:24:37 -0600

    Instalacion de paquetes de diseño

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 // src/main.jsx
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./assets/css/bootstrap.min.css"; // Si es personalizado
+import "./assets/css/fontawesome.min.css";
+import "./assets/css/line-awesome.min.css";
+import "./assets/css/magnific-popup.css";
+import "./assets/css/slick.css";
+// import "./index.scss";
 import App from "./App";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./assets/sass/main.scss";
 
 
 
